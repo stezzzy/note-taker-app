542bb2d364a8fb628e62eb8ba9f8a5faa46e82c0
Nick Stevens <nickstevens@Nicks-MacBook-Pro.local>
2022-09-15 15:17:11 -0400

    Building out HTML Routing

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,20 @@
+const { appendFile } = require('fs');
+const path = require('path');
+const router = require('express').Router();
+
+
+// Create our routes here
+
+// create route for notes.html
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+})
+
+// Use * to sen any other route hit back to homepage
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+
+// Initialize the app and create the port
+const app = express();
+const PORT = process.env.PORT || 3001
+
+// import our routes for express
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+app.use('/', htmlRoutes)
+
+
+
+// Starting server on PORT
+app.listen(PORT, () => console.log(`Listening to server on ${PORT}`))
